ddf400b81f74c074459e9c042555729c3c1e2f2d
SttackSite <sttacksite@gmail.com>
2026-03-19 11:50:00 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -494,7 +494,7 @@ st.markdown('</div>', unsafe_allow_html=True)
 # --- 3 & 4. SHOWCASE DE TEMPLATES (GRID 2D COM SCROLL) ---
 st.markdown('<div id="templates" style="padding: 120px 8%;">', unsafe_allow_html=True)
 st.markdown("""<h2>Navegue pelos modelos lateralmente e clique para ver cada <span class="serif-heavy"> template completo:</span></h2>
-<p style="color: var(--gold); font-family: 'Inter', sans-serif; font-size: 18px; margin-top: 20px;">Se fizer sentido pra você, comece a edição no final da página — rápido e fácil.</p>
+<p style="color: var(--gold); font-family: 'Inter', sans-serif; font-size: 18px; margin-top: 20px;">Se algum fizer sentido para o seu negócio, você pode começar a editar facilmente ao final da própria página do template.</p>
 <br><br>
 """, unsafe_allow_html=True)
 st.markdown("""
